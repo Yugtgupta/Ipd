060b2ec1bdd43c9202534193448fa7595caa5dc7
Yugtgupta <yugtgupta@gmail.com>
2024-09-02 03:47:30 +0530

    historical data route added and an intermediate page for historical data and candle data

--- index.js
+++ index.js
@@ -21,28 +21,6 @@ app.get('/', (req, res) => {
     }
 });
 
-
-
-app.get('/crypto/assets', async (req, res) => {
-    const options = {
-        method: 'GET',
-        url: 'https://api.coincap.io/v2/assets',
-        headers: {
-            'Accept-Encoding': 'gzip, deflate',
-            'Authorization': 'Bearer 1a793104-d0e8-4533-a983-c0972e36c157'
-        }
-    };
-
-    try {
-        const response = await axios(options);
-        console.log(response.data);
-        res.render('crypto', response.data);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Internal Server Error');
-    }
-});
-
 app.get('/stocks/all', async (req, res) => {
     try {
         const symbols = await nseIndia.getAllStockSymbols();
@@ -69,6 +47,59 @@ app.get('/stocks/:symbol', async (req, res) => {
     }
 });
 
+app.get('/crypto/assets', async (req, res) => {
+    const options = {
+        method: 'GET',
+        url: 'https://api.coincap.io/v2/assets',
+        headers: {
+            'Accept-Encoding': 'gzip, deflate',
+            'Authorization': 'Bearer 1a793104-d0e8-4533-a983-c0972e36c157'
+        }
+    };
+
+    try {
+        const response = await axios(options);
+        console.log(response.data);
+        res.render('crypto', response.data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+app.get('/crypto/assets/:id', async (req, res) => {
+    const id = req.params.id;
+    // console.log(id);
+    res.render('graph-data', {id: id});
+});
+
+
+
+app.get('/crypto/assets/:id/historical-data', async (req, res) => {
+    const id = req.params.id;
+    const options = {
+        method: 'GET',
+        url: `https://api.coincap.io/v2/assets/${id}/history?interval=d1`,
+        headers: {
+            'Accept-Encoding': 'gzip, deflate',
+            'Authorization': 'Bearer 1a793104-d0e8-4533-a983-c0972e36c157'
+        }
+    };
+
+    try {
+        const response = await axios(options);
+        console.log(response.data);
+        res.send(response.data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('An error occurred while fetching the data.');
+    }
+
+    // res.send('hello');
+});
+
+
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
-}); 
+});     
