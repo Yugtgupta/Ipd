4f7c291652946633d21c35435f190e7c44f5e11e
Yugtgupta <yugtgupta@gmail.com>
2024-09-02 01:37:37 +0530

    crypto data assets

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // index.js
 const express = require('express');
+const axios = require('axios');
 const app = express();
 const port = 3000;
+
 
 const { NseIndia } = require("stock-nse-india");
 const nseIndia = new NseIndia()
@@ -13,6 +15,28 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
     try {
         res.render('main');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+
+
+app.get('/crypto/assets', async (req, res) => {
+    const options = {
+        method: 'GET',
+        url: 'https://api.coincap.io/v2/assets',
+        headers: {
+            'Accept-Encoding': 'gzip, deflate',
+            'Authorization': 'Bearer 1a793104-d0e8-4533-a983-c0972e36c157'
+        }
+    };
+
+    try {
+        const response = await axios(options);
+        console.log(response.data);
+        res.render('crypto', response.data);
     } catch (error) {
         console.error(error);
         res.status(500).send('Internal Server Error');
